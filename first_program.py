7989777d70c80df60de87c2d968caff89696f379
Bill Le <choangbill.le@gmail.com>
2025-06-16 02:41:16 -0400

    update first_program and requirements.txt

--- first_program.py
+++ first_program.py
@@ -1,9 +1,14 @@
 from pymycobot import MyCobot280
 
+print("HELLO")
+
 mc = MyCobot280('/dev/ttyAMA0', 1000000)
+
+print("CONNECTED")
 
 # Gets the current angle of all joints
 angles = mc.get_angles()
+
 print(angles)
 
 mc.send_angles([0,0,0,0,0,0], 30)
