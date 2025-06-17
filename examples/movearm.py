0525a79fd69e77a07e7623d706da4382611a3763
Bill Le <choangbill.le@gmail.com>
2025-06-17 10:57:07 +1000

    reconfigure all files to match CYPIU project

---
+++ examples/movearm.py
@@ -0,0 +1,18 @@
+from pymycobot import MyCobot280
+
+print("HELLO")
+
+mc = MyCobot280("/dev/ttyAMA0", 1000000)
+
+print("CONNECTED")
+
+# Gets the current angle of all joints
+angles = mc.get_angles()
+
+print(angles)
+
+mc.send_angles([0, 0, 0, 0, 0, 0], 30)
+mc.send_angles([90, 0, 0, 0, 0, 0], 30)
+mc.send_angles([-90, 0, 0, 0, 0, 0], 30)
+
+print("arm moved!")
--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 from setuptools import find_packages, setup
 
-package_name = 'py_pubsub'
+package_name = "CYPIU"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='cypiu',
-    maintainer_email='choangbill.le@gmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="Bill Le",
+    maintainer_email="choangbill.le@gmail.com",
+    description="Can you pick it up?",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
