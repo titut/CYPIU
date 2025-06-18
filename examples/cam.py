3f043dcec98826c1c3f483d84af8348c0154f9ae
Bill Le <choangbill.le@gmail.com>
2025-06-18 15:02:04 +1000

    Camera Calibration

--- examples/cam.py
+++ examples/cam.py
@@ -1,27 +1,48 @@
 import cv2
+import os
 
-# Open the default camera (usually the first one)
+def fill_zero_num(num):
+    num_zeros = 3 - len(str(num))
+    return "0"*num_zeros + str(num)
+
+# Open the default camera (index 0). 
+# If you have multiple cameras, you might need to try different indices (1, 2, etc.).
 cap = cv2.VideoCapture(4)
 
+# Check if the camera opened successfully
 if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
 
+# Loop to continuously read and display frames
 while True:
     # Read a frame from the camera
     ret, frame = cap.read()
 
+    # If the frame was not successfully read, break the loop
     if not ret:
-        print("Error: Could not read frame.")
+        print("Error: Failed to grab frame.")
         break
 
-    # Display the frame in a window
+    # Display the captured frame in a window named 'Camera Feed'
     cv2.imshow('Camera Feed', frame)
 
-    # Break the loop if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Wait for 1 millisecond and check for a key press.
+    # If the 'q' key is pressed, exit the loop.
+    key_pressed = cv2.waitKey(1)
+    if key_pressed == ord('q'):
         break
 
-# Release the camera and destroy all windows
+    if key_pressed == ord('p'):
+        all_entries = os.listdir("../img/raw")
+        id = len(all_entries) + 1
+
+        filename = f"../img/raw/image{fill_zero_num(id)}.jpg"
+
+        # Save the frame as an image
+        cv2.imwrite(filename, frame)
+        print(f"Frame saved as {filename}")
+
+# Release the camera and destroy all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'launch'), glob('config/*'))
     ],
     install_requires=["setuptools"],
     zip_safe=True,
