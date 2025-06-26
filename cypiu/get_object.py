1f961f18feee053f4f56f65a5f07a79c8c4675be
Bill Le <choangbill.le@gmail.com>
2025-06-26 14:57:11 +1000

    edit tags.yaml and log extra things in object

--- cypiu/get_object.py
+++ cypiu/get_object.py
@@ -65,6 +65,9 @@ class GetObject(Node):
 
         t_sc = t_sb @ t_bc
         desired_ee = t_sc[0:3, 3]
+        self.get_logger().info(f"cur_angle = {self.current_angles}")
+        self.get_logger().info(f"t_sb = {t_sb}")
+        self.get_logger().info(f"t_bc = {t_bc}")
         self.get_logger().info(f"desired_ee = {desired_ee}")
 
         soln, status = inverse_kinematics(cur_angles, desired_ee)
