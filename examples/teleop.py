378ae2cbd123b584ecc81ef61b8efa451e2caaf6
Bill Le <choangbill.le@gmail.com>
2026-02-18 12:30:04 -0500

    teleop arm

--- examples/teleop.py
+++ examples/teleop.py
@@ -1,45 +1,46 @@
+import time
 from pydualsense import *
 from pymycobot import MyCobot280
-import modules.fk as fk
-from modules.util import deg2rad
+
+joystick_state_x = 0
+joystick_state_y = 0
+MAX_STICK = 128
+
+
+def joystick_handler(state_x, state_y):
+    global joystick_state_x, joystick_state_y
+    #  Flip it so that y is positive when the joystick is pushed upwards
+    state_y = -state_y
+
+    joystick_state_x = state_x / MAX_STICK
+    joystick_state_y = state_y / MAX_STICK
+
 
 mc = MyCobot280("/dev/ttyAMA0", 1000000)
 angles = mc.get_angles()
-mc.send_angles([0, 0, 0, 0, 0, 0], 30)
+mc.send_angles([0, 40, 0, 0, 0, 0], 30)
 
 # get dualsense instance
 dualsense = pydualsense()
 # initialize controller and connect
 dualsense.init()
 
-controlled_joint = 0
+dualsense.left_joystick_changed += joystick_handler
 
-while True:
-    if dualsense.state.cross:
-        controlled_joint = 1
-    elif dualsense.state.square:
-        controlled_joint = 2
-    elif dualsense.state.triangle:
-        controlled_joint = 3
-    elif dualsense.state.circle:
-        controlled_joint = 4
+while not dualsense.state.R1:
+    if joystick_state_x > 0.5:
+        print("Moving right")
+    elif joystick_state_x < -0.5:
+        print("Moving left")
+    else:
+        print("Not moving horizontally")
+    if joystick_state_y > 0.5:
+        print("Moving forward")
+    elif joystick_state_y < -0.5:
+        print("Moving backward")
+    else:
+        print("Not moving vertically")
 
-    if not (controlled_joint == 0):
-        if dualsense.state.R2:
-            mc.jog_angle(controlled_joint, 1, 10)
-        elif dualsense.state.L2:
-            mc.jog_angle(controlled_joint, 0, 10)
-        else:
-            if mc.is_moving() == 1:
-                mc.stop()
-
-    if dualsense.state.DpadUp:
-        cur_angle = mc.get_angles()
-        fk_soln = fk.forward_kinematics(deg2rad(cur_angle))
-        print(f"Cur angle: {cur_angle}")
-        print(f"Fk Soln: {fk_soln}")
-
-    if dualsense.state.R1:
-        break
+    time.sleep(0.5)
 
 dualsense.close()
